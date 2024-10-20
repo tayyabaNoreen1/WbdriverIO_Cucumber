e2ff155b68855b354e11434caf1dff73a7be54a6
tayyabaNoreen1 <tayyaba1.noreen@gmail.com>
2024-10-20 22:32:35 +0100

    resolved webdriver issue

--- features/login.demo.feature
+++ features/login.demo.feature
@@ -1,7 +1,14 @@
 Feature: Test login functionality
 
-  Scenario: Checklogin with valid credentials
+  Scenario Outline: Checklogin with valid credentials
+  #Scenario: Checklogin with valid credentials#
     Given user is on login page
-    When user enters username and password
+    When user enters <username> and <password>
     And clicks on login button
-    Then user is navigated to the home page
+   #Then user is navigated to the home page#
+    Then this <message> is displayed
+
+    Examples:
+      | username | password             | message                        |
+      | tomsmith | SuperSecretPassword! | You logged into a secure area! |
+      | user123  | pass123              | Your username is invalid!      |
--- features/step-definitions/login.demo.steps.js
+++ features/step-definitions/login.demo.steps.js
@@ -1,19 +1,32 @@
 
 const { Given, When, Then } = require('@wdio/cucumber-framework');
+const { expect, $ } = require('@wdio/globals')
 
 Given(/^user is on login page$/, async () => {
     await browser.url('https://the-internet.herokuapp.com/login')
 });
 
-When(/^user enters username and password$/, async () => {
-    await $('#username').setValue('tomsmith')
-    await $('#password').setValue('SuperSecretPassword!')
+// When(/^user enters username and password$/, async () => {
+//     await $('#username').setValue('tomsmith')
+//     await $('#password').setValue('SuperSecretPassword!')
+// });
+
+
+When(/^user enters (.*) and (.*)$/, async (username, password) => {
+    await $('#username').setValue(username)
+    await $('#password').setValue(password)
 });
+
 
 When(/^clicks on login button$/, async () => {
     await $('button[type="submit"]').click()
 });
 
-Then(/^user is navigated to the home page$/, async () => {
-    await expect($('#flash')).toHaveTextContaining('You logged into a secure area!')
+// Then(/^user is navigated to the home page$/, async () => {
+//     await expect($('#flash')).toHaveText(expect.stringContaining('You logged into a secure area!'))
+// });
+
+
+Then(/^this (.*) is displayed$/, async (message) => {
+    await expect($('#flash')).toHaveText(expect.stringContaining(message))
 });
